30cbb78c65698a386296b4e8497e5b7f6f222bba
ICOM STUDENTS <david.ortiz14@upr.edu>
2018-02-26 13:25:31 -0400

    lab2

---
+++ InheritanceLabStudents/src/orderedStructures/Arithmetic.java
@@ -0,0 +1,33 @@
+package orderedStructures;
+
+public class Arithmetic extends Progression {
+	private double commonDifference; 
+	
+	public Arithmetic(double firstValue, double commonDifference) { 
+		super(firstValue); 
+		this.commonDifference = commonDifference; 
+	}
+	
+	@Override
+	public double nextValue() {
+		current = current + commonDifference; 
+		return current;
+	}
+	@Override
+	public String toString(){
+				
+	return "Geom("+ (int)firstValue()+ ","+(int)commonDifference+")";
+		
+	}
+    @Override
+	
+	public double getTerm(int n) throws IndexOutOfBoundsException { 
+		if (n <= 0) 
+			throw new IndexOutOfBoundsException("printAllTerms: Invalid argument value = " + n); 
+
+		
+		return  (firstValue()) + (commonDifference)*(n-1);
+
+	}
+
+}
---
+++ InheritanceLabStudents/src/orderedStructures/Geometric.java
@@ -0,0 +1,34 @@
+package orderedStructures;
+
+public class Geometric extends Progression {
+
+	private double commonFactor; 
+	
+	public Geometric(double firstValue, double commonFactor) { 
+		super(firstValue); 
+		this.commonFactor = commonFactor; 
+	}
+	
+	@Override
+	public double nextValue() {
+		current = current * commonFactor; 
+		return current;
+	}
+	@Override
+	public String toString(){
+	
+				
+	return "Arith("+(int)firstValue()+ ","+(int)commonFactor+")";
+		
+	}
+	@Override
+	
+	public double getTerm(int n) throws IndexOutOfBoundsException { 
+		if (n <= 0) 
+			throw new IndexOutOfBoundsException("printAllTerms: Invalid argument value = " + n); 
+
+
+		return  (firstValue()) * Math.pow(commonFactor, (n-1));
+
+	}
+}
